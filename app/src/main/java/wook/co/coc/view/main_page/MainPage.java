095a996b27f34b1736a1b36738e1855b373774a5
hankgood95 <hankgood958@gmail.com>
2021-07-25 19:11:52 +0900

    스플래시 화면 만들었음

--- app/src/main/java/wook/co/coc/view/main_page/MainPage.java
+++ app/src/main/java/wook/co/coc/view/main_page/MainPage.java
@@ -1,14 +1,16 @@
-package wook.co.coc;
+package wook.co.coc.view.main_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import wook.co.coc.R;
+
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainpage_mainpage);
     }
 }

7c76d065d3dec3b6bea1510f7a497346ecddcc7c
hankgood95 <hankgood958@gmail.com>
2021-07-25 22:28:39 +0900

    바텀네비게이션 만들고 fragment파일은 만들었지만 databinding은 안했음

---
+++ app/src/main/java/wook/co/coc/view/bottom_nav/HomeFragment.java
@@ -0,0 +1,28 @@
+package wook.co.coc.view.bottom_nav;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import wook.co.coc.R;
+
+public class HomeFragment extends Fragment {
+
+    private View view;
+    private String TAG = "HomeFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG,"onCreatView");
+
+        view = inflater.inflate(R.layout.mainpage_mainpage,container,false);
+        return view;
+    }
+}
